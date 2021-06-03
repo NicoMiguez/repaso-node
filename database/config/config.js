58579b34f6e75b67bc6c1b85407b2ec3ae50567a
NicoMiguez <migueznico@gmail.com>
2021-06-03 18:07:11 -0300

    database y header

--- database/config/config.js
+++ database/config/config.js
@@ -1,9 +1,10 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": null,
-    "database": "movies-db",
+    "password": '',
+    "database": "movies_db",
     "host": "127.0.0.1",
+    "port": '3306',
     "dialect": "mysql",
     "opreatorAliases": false
   },
